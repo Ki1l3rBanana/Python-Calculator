0645dc8a7eb502f949aabdbc1bacd4cea976aaa0
UserCodeManager <101026064+UserCodeManager@users.noreply.github.com>
2022-07-10 13:41:20 -0400

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,21 @@
+first = float(input("Enter first Number => "))
+sec = float(input("Enter Second Number => "))
+opr = str(input("Enter Operation (+, -, *, /) => "))
+
+if opr == "+":
+    total = first + sec
+elif opr == "-":
+    total = first - sec
+elif opr == "*":
+    total = first * sec
+elif opr == "/":
+    total = first / sec
+else:
+    total = str("Please Enter a Valid Operation")
+
+
+
+print (total)
+
+
+
